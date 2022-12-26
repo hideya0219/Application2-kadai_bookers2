ccfe60db40f1aeecfcf42872748439c7e63b2cfa
hideya0219 <119572041+hideya0219@users.noreply.github.com>
2022-12-26 10:20:46 +0000

    [add]introductionカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2022_12_26_091221) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
-    t.string "introduction"
+    t.text "introduction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
