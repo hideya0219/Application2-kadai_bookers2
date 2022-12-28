027ae05d5421f7df277a014c0c4a4849f200969c
hideya0219 <119572041+hideya0219@users.noreply.github.com>
2022-12-28 22:17:26 +0000

    [finish]ファイルの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  resources :books, only: [:index, :show, :edit]
   devise_for :users
   root to: 'homes#top'
   get 'homes/about', as: 'about'
